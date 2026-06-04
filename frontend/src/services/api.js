07e3ca165e3fd51483c9a0868f857ddc68d72f4f
Sitaram Hembrom <sitaram.hembrom123@gmail.com>
2026-06-05 01:05:25 +0530

    Update production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,8 +3,9 @@ import axios from "axios";
 const API =
 axios.create({
 
-baseURL:
-"http://localhost:5000"
+// baseURL:
+// "http://localhost:5000"
+baseURL: "https://ai-daily-expense-tracker.onrender.com"
 
 });
 
